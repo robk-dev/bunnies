d22cda4b8b79a97c9a4da5dd1166e7c6521b084f
Robert Kardjaliev <robk-dev@users.noreply.github.com>
2020-11-14 23:44:08 +0000

    refactored html by extending layout; added 4 pages other than home; added active for routes

--- app/routes.py
+++ app/routes.py
@@ -1,24 +1,83 @@
 from app import app
 from flask import render_template
 
+
 @app.route('/')
 @app.route('/index')
 @app.route('/home')
 def index():
-    return render_template('index.html', login=True)
+    return render_template('index.html', login=True, index=True)
+
 
 @app.route("/login")
 def login():
-    return render_template("login.html", login=False)
+    return render_template("login.html", login=True)
+
 
 @app.route("/bunnies")
 def bunnies():
-    return render_template("bunnies.html", login=False)
+    bunnies_data = [{
+        "bunny_id": "sexy-bunny-19",
+        "name": "John Carrotson",
+        "bio": "A hopper and a chopper for anything hoppening.",
+        "score": "6.5/10",
+        "origin": "New York"
+    }, {
+        "bunny_id": "i-like-carrots",
+        "name": "Lolita Bunny",
+        "bio": "Likes hanging out at the carrot stand in the mall.",
+        "score": "8.5/10",
+        "origin": "Washington"
+    }, {
+        "bunny_id": "c4rr07 4r3al",
+        "name": "Anonymous",
+        "bio": "Can burrow into any network patch.",
+        "score": "??/??",
+        "origin": "Dark Web"
+    }, {
+        "bunny_id": "fluffy",
+        "name": "Fluffy McFluffer",
+        "bio": "The fluffliest fluff to ever be fluffed.",
+        "score": "9.5/10",
+        "origin": "Mexico"
+    }, {
+        "bunny_id": "bugs",
+        "name": "Bugs Bunny",
+        "bio": "Always causes bugs and breaks production.",
+        "score": "3/10",
+        "origin": "Burrow End"
+    }]
+
+    # print(bunnies_data[0])
+    return render_template("bunnies.html", data=bunnies_data, bunnies=True)
+
 
 @app.route("/carrots")
 def carrots():
-    return render_template("carrots.html", login=False)
+    carrots_data = [{
+        "carrot_id": "trump-carrot",
+        "name": "34 carat carrot",
+        "description": "Carrot made of solid gold.",
+        "taste": "0/10",
+        "texture": "1/10"
+    }, {
+        "carrot_id": "le-old-mcdonald",
+        "name": "Old Yi Faithful Carrot",
+        "description": "For the senior hoppers' choppers.",
+        "taste": "3/10",
+        "texture": "7/10"
+    }, {
+        "carrot_id": "standard",
+        "name": "Free Range Carrots",
+        "description": "Organically grown, fresh picked, free-range carrots.",
+        "taste": "8/10",
+        "texture": "9/10"
+    }]
+
+    # print(carrots_data[0])
+    return render_template("carrots.html", data=carrots_data, carrots=True)
+
 
 @app.route("/register")
 def register():
-    return render_template("register.html", login=False)
+    return render_template("register.html", login=False, register=True)
